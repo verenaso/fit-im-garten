510df2c5323634bdd71c255917f73bf1cee9733e
Verenaso <verena.soethe@gmail.com>
2026-01-23 18:03:54 +0100

    provider fix

--- src/app/_components/Providers.js
+++ src/app/_components/Providers.js
@@ -1,6 +1,6 @@
 "use client";
 
-import AuthProvider from "./AuthProvider";
+import { AuthProvider } from "./AuthProvider";
 
 export default function Providers({ children }) {
   return <AuthProvider>{children}</AuthProvider>;
